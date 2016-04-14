f5aa662619ac5371604342a04883dd526f7681d2
Rafael Salguero <rafaelsalgueroiturrios@gmail.com>
2016-04-13 17:40:03 -0700

    Initial commit

---
+++ EntityFrameworkInterfaces/IDatabase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkInterfaces
+{
+    /// <summary>
+    /// Un objeto capaz de proveer de ITables
+    /// </summary>
+    public interface IDatabase : IDisposable
+    {
+        ITable<T> Set<T>() where T : class;
+        /// <summary>
+        /// Guarda los cambios en la base de datos
+        /// </summary>
+        void SaveChanges();
+    }
+
+    /// <summary>
+    /// Implementacion de IDatabase a partir de un DbContext
+    /// </summary>
+    public class ContextDatabase : IDatabase
+    {
+        public ContextDatabase(DbContext Db)
+        {
+            this.Db = Db;
+        }
+
+        private readonly DbContext Db;
+
+        public ITable<T> Set<T>() where T : class
+        {
+            return new DbSetTable<T>(Db.Set<T>());
+        }
+
+        public void Dispose()
+        {
+            Db.Dispose();
+        }
+
+        public void SaveChanges()
+        {
+            Db.SaveChanges();
+        }
+    }
+
+    /// <summary>
+    /// Implementación de IDatabase a partir de un objeto que tiene como propiedades objetos de tipo List&lt;T&gt;
+    /// </summary>
+    public class InMemoryDatabase : IDatabase
+    {
+        /// <summary>
+        /// Crea un IDatabase a partir de un objeto con propiedades de tipo List
+        /// </summary>
+        /// <param name="ObjectWithLists">Para cada propiedad del objeto que sea de tipo tipo List&lt;T&gt, se crea un Set de la base de datos el cual tendra los datos que tienen la lista, y las modificaciones a las tablas afectaran a su vez a las listas originales</param>
+        public InMemoryDatabase(object ObjectWithLists)
+        {
+            this.lists = ObjectWithLists.GetType().GetProperties()
+                .Where(x => x.PropertyType.IsGenericType && x.PropertyType.GetGenericTypeDefinition() == typeof(List<>))
+                .ToDictionary(x => x.PropertyType.GetGenericArguments()[0], x => x.GetValue(ObjectWithLists));
+        }
+
+        private Dictionary<Type, object> lists;
+
+        public ITable<T> Set<T>() where T : class
+        {
+            return new ListTable<T>((List<T>)lists[typeof(T)]);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void SaveChanges()
+        {
+            //Falta implementar la logica del SaveChanges en el mock
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ EntityFrameworkInterfaces/ITable.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkInterfaces
+{
+    /// <summary>
+    /// Una interfaz que envuelve a un DbSet para su posible uso en pruebas unitarias.
+    /// Los metodos que deseen manipular una tabla deberan depender de esta interfaz en lugar de un DbSet.
+    /// Los metodos que solo filtren resultados deberan depender de IQueryable
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface ITable<T>
+    {
+        /// <summary>
+        /// Borra una entidad de la tabla
+        /// </summary>
+        /// <param name="Entity"></param>
+        void Remove(T Entity);
+
+        /// <summary>
+        /// Agrega un elemento a la tabla
+        /// </summary>
+        /// <param name="Entity"></param>
+        void Add(T Entity);
+
+        /// <summary>
+        /// Obtiene todos los elementos de la tabla
+        /// </summary>
+        IQueryable<T> All { get; }
+    }
+    /// <summary>
+    /// Implementación de un ITable a partir de un List
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ListTable<T> : ITable<T>
+    {
+        public ListTable(IList<T> List)
+        {
+            this.list = List;
+        }
+        readonly IList<T> list;
+
+        IQueryable<T> ITable<T>.All
+        {
+            get
+            {
+                return list.AsQueryable();
+            }
+        }
+
+        void ITable<T>.Remove(T Entity)
+        {
+            list.Remove(Entity);
+        }
+
+        void ITable<T>.Add(T Entity)
+        {
+            list.Add(Entity);
+        }
+    }
+
+    /// <summary>
+    /// Implementación de un ITable a partir de un DbSet
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class DbSetTable<T> : ITable<T>
+        where T : class
+    {
+        public DbSetTable(DbSet<T> Set)
+        {
+            this.Set = Set;
+        }
+        private readonly DbSet<T> Set;
+
+        IQueryable<T> ITable<T>.All
+        {
+            get
+            {
+                return Set;
+            }
+        }
+
+        void ITable<T>.Add(T Entity)
+        {
+            Set.Add(Entity);
+        }
+
+        void ITable<T>.Remove(T Entity)
+        {
+            Set.Remove(Entity);
+        }
+    }
+}
